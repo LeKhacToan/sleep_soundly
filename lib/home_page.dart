35364e4090ff5f86600bbd5fc99ed2da57232b78
toanlk13 <toanlk13@ghtk.co>
2022-11-14 16:11:14 +0700

    feat(*): play audio

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -20,14 +21,13 @@ class _HomePage extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-  
+
     audioPlayer.setReleaseMode(ReleaseMode.loop);
     audioPlayer.onPlayerStateChanged.listen((event) {
       setState(() {
         isPlaying = event == PlayerState.playing;
       });
     });
-    playAudio();
   }
 
   @override
@@ -36,20 +36,25 @@ class _HomePage extends State<HomePage> {
     super.dispose();
   }
 
-  void handleSelectSound(value) {
+  Future<void> handleSelectSound(value) async {
     setState(() {
       item = value;
     });
-    playAudio();
-  }
-
-  Future playAudio() async {
     audioPlayer.pause();
     audioPlayer.setSourceAsset(item['mp3']);
     await audioPlayer.resume();
   }
 
-  _showMyDialog() {
+  Future<void> controlPlayer() async {
+    if (isPlaying) {
+      audioPlayer.pause();
+    } else {
+      audioPlayer.setSourceAsset(item['mp3']);
+      await audioPlayer.resume();
+    }
+  }
+
+  showSettingDialog() {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -73,10 +78,34 @@ class _HomePage extends State<HomePage> {
               width: double.infinity,
               fit: BoxFit.cover,
             ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showMyDialog,
-        tooltip: 'Setting',
-        child: const Icon(Icons.settings),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                heroTag: 'play',
+                onPressed: controlPlayer,
+                tooltip: 'Setting',
+                backgroundColor: Colors.white,
+                child: Icon(
+                  isPlaying
+                      ? CupertinoIcons.pause_fill
+                      : CupertinoIcons.play_arrow_solid,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              const SizedBox(height: 16),
+              FloatingActionButton(
+                heroTag: 'seting',
+                onPressed: showSettingDialog,
+                tooltip: 'Setting',
+                child: const Icon(Icons.settings),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
